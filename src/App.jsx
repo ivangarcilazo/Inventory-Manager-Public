9676b7c857a7c9352c35da62af9366c4756f22db
Ivan Lautaro Garcilazo <ivangarcilazo.dv@gmail.com>
2023-06-27 20:46:12 -0300

    fix/Error

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { HashRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Login from './pages/Login/Login'
 import Home from './pages/Home/Home'
 import Layout from './Components/Layout/Layout'
